d2311017e6f687e6e6f2b73004e37f8e005635ce
chickenbreeder <chickenbreeder@users.noreply.github.com>
2021-02-25 23:01:15 +0100

    Export all available modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
-mod raw_class;
-mod constant_pool;
-mod access_flags;
-mod attribute;
-mod error;
-mod field;
+pub mod raw_class;
+pub mod constant_pool;
+pub mod access_flags;
+pub mod attribute;
+pub mod error;
+pub mod field;
 
 pub use raw_class::RawClass;
+pub use constant_pool::{Constant, ConstantTag, ReferenceKind};
+pub use field::Field;
+pub use attribute::Attribute;
